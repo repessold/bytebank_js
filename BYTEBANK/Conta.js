ab296fe19630cc887353040381ea985c808bb77c
Rafaela Repessold C Andrade <rafaela.casrep@gmail.com>
2023-03-31 10:58:32 -0300

    Herança

---
+++ BYTEBANK/Conta.js
@@ -0,0 +1,27 @@
+export class Conta{
+    constructor(saldoInicial, cliente, agencia) {
+        this._saldo = saldoInicial;
+        this._cliente = cliente;
+        this._agencia = agencia;
+    }
+
+    sacar(valor) {
+        if (this._saldo >= valor) {
+            this._saldo -= valor;
+            return valor;
+        }
+    }
+
+    depositar(valor) {
+        if (valor <= 100) {
+            return;
+        }
+        this._saldo += valor;
+    }
+
+    tranferir(valor, conta) {
+
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
+    }
+}
