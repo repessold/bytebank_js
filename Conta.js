495e1f35e6417b46e7981d145de8eab7c15f4e41
Rafaela Repessold C Andrade <rafaela.casrep@gmail.com>
2023-04-10 12:01:22 -0300

    exclui pastas exclui repeticao de codigo

--- Conta.js
+++ Conta.js
@@ -1,47 +1,45 @@
-export class Conta{
+import { Cliente } from "./Cliente";
+
+export class Conta {
     constructor(saldoInicial, cliente, agencia) {
         this._saldo = saldoInicial;
         this._cliente = cliente;
         this._agencia = agencia;
-        this._tipo = tipo;      
+        this._tipo = tipo;
     } //utilizar o constructor ao invés de declarar vários objetos
 
-    set cliente(novoValor){
-        if(novoValor instanceof Cliente){
-           this._cliente = novoValor; 
+    set cliente(novoValor) {
+        if (novoValor instanceof Cliente) {
+            this._cliente = novoValor;
         }
-        
+
     }
-    get cliente(){
+    get cliente() {
         return this._cliente;
     }
 
- 
-    get saldo(){
+
+    get saldo() {
         return this._saldo;
     }
 
     sacar(valor) {
 
-        let = taxa = 1
+        let taxa = 1
+
         const valorSacado = taxa * valor;
         if (this._saldo >= valorSacado) {
             this._saldo -= valorSacado;
             return valorSacado;
-     }
- }
+        }
+    }
 
     depositar(valor) {
-        if (valor <= 100) {
-            return;
-        }
+
         this._saldo += valor;
     }
 
     tranferir(valor, conta) {
-        if(this._tipo == "salario"){
-            return;
-        }
 
         const valorSacado = this.sacar(valor);
         conta.depositar(valorSacado);
