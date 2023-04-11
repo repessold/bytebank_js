fb98c3fbbded37649d09ac4729f765e339b98bfc
Rafaela Repessold C Andrade <rafaela.casrep@gmail.com>
2023-04-11 07:55:05 -0300

    Descontinuando

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -4,6 +4,7 @@ import { Conta } from "./Conta.js";
 export class ContaCorrente extends Conta {
     static numeroDeContas = 0;
     constructor(cliente, agencia) {
+        super(0, cliente, agencia);
         ContaCorrente.numeroDeContas += 1;
     }
 
