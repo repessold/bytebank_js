8d34e5aa50f45084d283ffda5bd82c65cc0959b7
Rafaela Repessold C Andrade <rafaela.casrep@gmail.com>
2023-04-12 11:57:48 -0300

    defini a autenticacao

--- funcionarios/Funcionario.js
+++ funcionarios/Funcionario.js
@@ -1,9 +1,19 @@
-export class Funcionario{
-    constructor(nome, _salario, _cpf){
-        this._nome= nome;
-        this._salario= salario;
-        this._cpf= cpf;
+export class Funcionario {
+    constructor(nome, _salario, _cpf) {
+        this._nome = nome;
+        this._salario = _salario;
+        this._cpf = _cpf;
 
-        this._bonificacao=1;
+        this._bonificacao = 1;
+        this._senha;
     }
+    
+    get senha(){
+        return this._senha;
+    }
+
+    cadastrarSenha(_senha) {
+        this._senha = _senha;
+    }
+
 }
--- index.js
+++ index.js
@@ -1,19 +1,11 @@
 import { Cliente } from "./Cliente.js";
-import { ContaCorrente } from "./Conta/ContaCorrente.js";
-import { ContaPoupanca } from "./Conta/ContaPoupanca.js";
-import { ContaSalario } from "./Conta/ContaSalario.js"
+import { Gerente } from "./funcionarios/gerente.js";
+import { Diretor } from "./funcionarios/diretor.js";
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
-const cliente1 = new Cliente("Ricardo", 11122233309);
+const diretor = new Diretor("Rodrigo,10000,12345678900");
+const gerente = new Gerente("Ricardo, 5000,12378945601");
 
-const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
-const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
-const contaSalario = new ContaSalario(cliente1);
-contaCorrenteRicardo.depositar(500);
-contaCorrenteRicardo.sacar(100);
+const estaLogado = SistemaAutenticacao.login(diretor, "123456789");
 
-
-console.log(contaSalario);
-
-//console.log(conta);
-//console.log(contaPoupanca);
-//console.log(contaCorrenteRicardo)
+console.log(estaLogado);
