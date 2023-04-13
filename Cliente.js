bf25335560f7044ca449c3e73f5580ab4a0d6cb8
Rafaela Repessold C Andrade <rafaela.casrep@gmail.com>
2023-04-13 10:58:22 -0300

    Verificacao de senha

--- Cliente.js
+++ Cliente.js
@@ -6,7 +6,11 @@ export class Cliente{
     constructor(nome, cpf, senha){
         this.nome = nome;
         this._cpf = cpf;
-        this._senha= senha;
+        this._senha= senha;        
     }
-    
+
+    autenticar(){
+        return true;
+    }
+
 }
